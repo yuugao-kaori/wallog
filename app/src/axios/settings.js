9c188871bbd7f2b89570dcbdd3ff55ec982e056a
yuugao-kaori <yuugaokaori@gmail.com>
2024-11-22 19:35:00 +0900

    NextJS_test

--- app/src/axios/settings.js
+++
@@ -1,7 +0,0 @@
-
-import axios from 'axios';
-
-axios.defaults.baseURL = 'http://192.168.1.148:25000';
-axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-export default axios;
--- app/src/pages/Blog.jsx
+++
@@ -1,9 +0,0 @@
-// ./pages/test000.jsx
-
-import React from 'react';
-
-const Test000 = () => {
-  return <h1>Blog機能は実装中です！</h1>;
-};
-
-export default Test000;
