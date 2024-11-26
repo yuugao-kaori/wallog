774daf5d71795f65bf1e0b157e6a411118e8da53
yuugao-kaori <yuugaokaori@gmail.com>
2024-11-26 19:33:18 +0900

    テーマ変更ボタンをちゃんと表示できるように修正

--- app/test_app/src/app/ThemeButton.tsx
+++ app/test_app/src/app/ThemeButton.tsx
@@ -1,4 +1,3 @@
-
 'use client'
 
 import { useTheme } from './ThemeContext';
@@ -9,7 +8,7 @@ export default function ThemeButton() {
   return (
     <button
       onClick={toggleTheme}
-      className="fixed top-4 right-4 px-4 py-2 rounded-md bg-gray-200 dark:bg-gray-800"
+      className="fixed top-4 right-4 px-4 py-2 rounded-md bg-gray-200 dark:bg-gray-800 z-50"
     >
       {theme === 'light' ? '🌙' : '☀️'}
     </button>
