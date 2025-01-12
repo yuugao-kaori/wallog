f901f3cd7ede5d2f2d4db60cf31c898dbe8be351
yuugao-kaori <yuugaokaori@gmail.com>
2025-01-12 12:58:12 +0900

    Dev 2025.1.3.0004B - Nginxの

--- app/test_app/src/app/NavBar.tsx
+++ app/test_app/src/app/NavBar.tsx
@@ -1,5 +1,5 @@
 'use client'
-const version = 'Dev 2025.1.3.0004A'
+const version = 'Dev 2025.1.3.0004B'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useState, useEffect, useMemo } from 'react'
